72570612e2d6281059be4918ce9a8ffa69129a9a
Biktor <sleepyh@gmail.com>
2015-11-08 19:40:29 +0100

    cleanup

--- configs/swap.sh
+++ configs/swap.sh
@@ -1,8 +1,8 @@
-#!/bin/ksh
+#!/system/bin/sh
 FILE=/data/swapdir/swapfile
 if [[ ! -f $FILE ]];
 then
 mkdir /data/swapdir && dd if=/dev/zero of=/data/swapdir/swapfile bs=1m count=512 && mkswap /data/swapdir/swapfile
-else
-swapon /data/swapdir/swapfile && sysctl -w vm.swappiness=10
 fi
+swapon /data/swapdir/swapfile && sysctl -w vm.swappiness=10
+
